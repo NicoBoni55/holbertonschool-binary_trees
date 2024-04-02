919b563700ffaf82b6204335ab46d7e928cc5913
NicoBoni55 <bonifrank55@gmail.com>
2024-04-02 14:56:18 +0000

    binary_trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,8 +15,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	while (newnode != NULL)
 	{
 	newnode->n = value;
-	newnode->parent = parent;
-	
+	newnode->parent = ::parent;
+
 	return (newnode);
 	}
 	return (NULL);
