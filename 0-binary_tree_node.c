dcb99a69d37b06b9f5da8250946ac0312c571f6c
NicoBoni55 <146758381+NicoBoni55@users.noreply.github.com>
2024-04-02 14:12:13 -0300

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	while (newnode != NULL)
 	{
 	newnode->n = value;
-	newnode->parent = ::parent;
+	newnode->parent = parent;
 
 	return (newnode);
 	}
