9adb33f2bb90f70d32e5fc159cb72e0c60e66c11
NicoBoni55 <bonifrank55@gmail.com>
2024-04-02 17:52:13 +0000

    binary_trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -16,6 +16,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	{
 	newnode->n = value;
 	newnode->parent = parent;
+	newnode->right = NULL;
+	newnode->left = NULL;
 
 	return (newnode);
 	}
