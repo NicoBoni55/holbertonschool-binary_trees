f53ab18957acdacff372d8a9940030b7500e20e7
AgusRodriguez15 <8670@holbertonstudents.com>
2024-04-03 18:06:01 +0000

    11-binary_tree_size.cCorrectBetty

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 /**
  * binary_tree_size - Entry point
+ * @tree: arbol
  *
  * Return: Always 0 (Success)
  */
@@ -10,12 +11,14 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	size_t b = 0;
 
 	if (tree == NULL)
-		return (0);
-	else 
 	{
+		return (0);
+	}
+
+
 		a = binary_tree_size(tree->left);
 		b = binary_tree_size(tree->right);
+
 		return (a + b + 1);
-	}
 }
 
