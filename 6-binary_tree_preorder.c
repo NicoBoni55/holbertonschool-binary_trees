f30cf971ae1ebb2edbb8c81004b7cb7a409778c5
NicoBoni55 <bonifrank55@gmail.com>
2024-04-03 15:34:17 +0000

    binary_trees

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - 
+ * @tree : 
+ *
+ * Return: Always 0 (Success)
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	
