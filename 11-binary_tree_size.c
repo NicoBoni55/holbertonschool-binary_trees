e41316198ed8266de05d5ee25f015a4b1036100d
AgusRodriguez15 <8670@holbertonstudents.com>
2024-04-03 17:53:22 +0000

    11-binary_tree_size.c

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t a = 0;
+	size_t b = 0;
+
+	if (tree == NULL)
+		return (0);
+	else 
+	{
+		a = binary_tree_size(tree->left);
+		b = binary_tree_size(tree->right);
+		return (a + b + 1);
+	}
+}
+
