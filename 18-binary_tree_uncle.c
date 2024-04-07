1dad588b3ae0c7e9cd68fde148aab797d9bfccfc
AgusRodriguez15 <8670@holbertonstudents.com>
2024-04-07 15:15:49 +0000

    binaryTREEExcercise18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - Busca el tioo
+ * @node: nodo
+ *
+ * Return: Always 0 (Success)
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+	{
+	binary_tree_t *a;
+	binary_tree_t *parent = NULL;
+	binary_tree_t *uncle = NULL;
+
+	if (node == NULL || node->parent->parent == NULL)
+		return (NULL);
+
+	parent = node->parent;
+	uncle = parent->parent;
+
+	if (uncle->left == parent)
+	uncle = parent->parent->right;
+	else
+	uncle = parent->parent->left;
+
+	a = uncle;
+
+	return (a);
+}
